063ba6584af20d38d7f692a342f07935f108bee9
JOTAI <jotabeles.web@gmail.com>
2016-12-06 19:53:59 -0500

    prueba correccion 8

--- Template/index/print.datasensor.js
+++ Template/index/print.datasensor.js
@@ -19,7 +19,7 @@ $(function () {
       //var d1 = new Date(data.Data.Date[a]);
       var d1 = new Date();
       var d2 = Date.UTC(2013,5,2,11,20,50);
-      if(a==2){ alert(data.Data.Date[a]); }
+      if(a==2){ alert(d1.getTime()); }
 
       var d = new Date(data.Data.Date[a]).getTime();
       Data.push([d, data.Data.Value[a]]);
