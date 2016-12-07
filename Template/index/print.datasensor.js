4fe3afba10840cba7c52cf84a4a32b62b1f012a0
JOTAI <jotabeles.web@gmail.com>
2016-12-06 19:07:52 -0500

    prueba correccion 3

--- Template/index/print.datasensor.js
+++ Template/index/print.datasensor.js
@@ -17,7 +17,7 @@ $(function () {
     // Generando la data para la grafica
     for (var a=0;a<data.Data.Value.length ;a++){
       var d1 = new Date(data.Data.Date[a]);
-      var d2 = Date.UTC(2013,5,2,d1.getHours(),d1.getMinutes(),d1.getSeconds());
+      var d2 = Date.UTC(2013,5,2);
       if(a==2){alert(d2);}
 
       var d = new Date(data.Data.Date[a]).getTime();
